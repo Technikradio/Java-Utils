83a4ddca2b679efdbcdd1942b5f860244cf9496d
Doralitze <leondietrich@leons-imac-3.speedport_w_921v_1_34_000>
2014-05-18 10:16:38 +0200

    fix some mistakes

--- src/org/technikradio/universal_tools/SettingsReader.java
+++ src/org/technikradio/universal_tools/SettingsReader.java
@@ -3,51 +3,63 @@ package org.technikradio.universal_tools;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import org.technikradio.cui.Console;
 
 public class SettingsReader {
 
-	private Dictionary<String, String> values;
+	private Hashtable<String, String> values;
 
 	public SettingsReader() {
 		super();
-		values = new Dictionary<String, String>();
+		values = new Hashtable<String, String>();
 	}
-	
+
 	public SettingsReader(String file) throws FileNotFoundException {
 		super();
-		values = new Dictionary<String, String>();
+		values = new Hashtable<String, String>();
 		loadFile(file);
 	}
-	
+
 	public SettingsReader(FileReader file) throws FileNotFoundException {
 		super();
-		values = new Dictionary<String, String>();
+		values = new Hashtable<String, String>();
 		loadFile(file);
 	}
-	
+
 	/**
-	 * This loads an config file
-	 * @param file : the file to load 
+	 * This loads an configuration file
+	 * 
+	 * @param file
+	 *            : the file to load
 	 * @return the parent object
 	 */
-	public SettingsReader loadFile(String file) throws FileNotFoundException{
+	public SettingsReader loadFile(String file) throws FileNotFoundException {
 		this.loadFile(new FileReader(file));
 		return this;
 	}
-	
+
 	/**
-	 * This loads an config file
-	 * @param file : the file to load 
+	 * This loads an configuration file
+	 * 
+	 * @param file
+	 *            : the file to load
 	 * @return the parent object
-	 */ 
-	public SettingsReader loadFile(FileReader file) throws FileNotFoundException {
-		try{
+	 */
+	public SettingsReader loadFile(FileReader file)
+			throws FileNotFoundException {
+		BufferedReader reader = null;
+		try {
 			reader = new BufferedReader(file);
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				String s1 = line.split("=")[0];
 				String s2 = line.split("=")[1];
-				values.add(s1, s2);
+				values.put(s1, s2);
 			}
 		} catch (FileNotFoundException e) {
 			throw e;
@@ -58,7 +70,8 @@ public class SettingsReader {
 				try {
 					reader.close();
 				} catch (IOException e) {
-					Console.Log(Console.LogType.Error, this, "Can´t close BufferedReader");
+					Console.log(Console.LogType.Error, this,
+							"Can´t close BufferedReader");
 					e.printStackTrace();
 				}
 			}
@@ -66,23 +79,29 @@ public class SettingsReader {
 		return this;
 	}
 
-	public SettingsReader saveFile(String file){
+	public SettingsReader saveFile(String file) throws FileNotFoundException{
 		PrintWriter pWriter = null;
-        	try {
-        		for(int i = 0; i < values.size() i++){
-        			
-        		}
-        	} catch (IOException e) {
-         		Console.Log(Console.LogType.Error, this, "Fail to save settings");
-            		e.printStackTrace();
+		try {
+			pWriter = new PrintWriter(file);
+			for (Enumeration<String> e = values.keys(); e.hasMoreElements();) {
+				String s = e.nextElement();
+				pWriter.print(s + "=" + values.get(s));
+				pWriter.flush();
+			}
+			pWriter.close();
+		} catch (IOException e) {
+			Console.log(Console.LogType.Error, this, "Fail to save settings");
+			e.printStackTrace();
 		} finally {
-            		if (pWriter != null)
-                		pWriter.flush();
-        	}
-		return this;	
+			if (pWriter != null)
+				pWriter.flush();
+		}
+		return this;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
